60a483aa8eb5de1bf0c4600ef5a45cf7f3e988ab
Anna <34191811+404unf@users.noreply.github.com>
2023-04-24 21:19:17 -0700

    Test driven non-Object Object

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {"name":"Hansel Glutton", "address":"123 Edge of the Woods"}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployeeData = {...employee}
+    newEmployeeData[key] = value
+    return newEmployeeData
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const cloneEmployee = {...employee}
+    delete cloneEmployee[key]
+    return cloneEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee   
+}
